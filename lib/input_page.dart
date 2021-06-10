9542f568e717a537872f4958590148315bb099de
Wentao Wu <wentao3970@gmail.com>
2021-06-09 23:41:25 -0400

    added a Slider to choose height.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-const backgroundColor = Color(0xFF090F3E);
+const backgroundColor = Color(0xFF0E112F);
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,6 +9,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  int height = 175;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,12 +47,45 @@ class _InputPageState extends State<InputPage> {
                     ),
                   ),
                 ],
-              ),
+              ).p12(),
             ),
             Expanded(
               child: Container(
-                child: 'child'.text.makeCentered(),
-              ),
+                color: Color(0xFF16232F),
+                child: Column(
+                  children: [
+                    'HEIGHT'.text.headline6(context).gray500.make(),
+                    Row(
+                      children: [
+                        height
+                            .toString()
+                            .text
+                            .headline2(context)
+                            .white
+                            .bold
+                            .make(),
+                        'cm'.text.subtitle1(context).gray500.make(),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 130.0,
+                      max: 230.0,
+                      activeColor: Colors.pink,
+                      inactiveColor: Colors.grey,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ).p12(),
             ),
             Expanded(
                 child: Row(
